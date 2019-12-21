cb71255f9e1c43699dcb108c3b47e9e127e23949
MinexAutomation <minex.automation@gmail.com>
2019-12-21 13:09:47 -0800

    Fixed method name.

--- source/R5T.Larissa.Configuration/Code/Services/Definitions/ISvnExecutableFilePathProvider.cs
+++ source/R5T.Larissa.Configuration/Code/Services/Definitions/ISvnExecutableFilePathProvider.cs
@@ -5,6 +5,6 @@ namespace R5T.Larissa.Configuration
 {
     public interface ISvnExecutableFilePathProvider
     {
-        string GetSvnExecutableFilePathProvider();
+        string GetSvnExecutableFilePath();
     }
 }
