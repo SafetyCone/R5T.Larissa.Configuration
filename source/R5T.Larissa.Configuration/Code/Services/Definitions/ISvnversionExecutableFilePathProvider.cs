fc3bc0c598f31f296e1719977d723303fe8c2601
MinexAutomation <minex.automation@gmail.com>
2019-12-21 22:33:59 -0800

    Actually adding the Svnversion executable file path provider service definition interface type.

---
+++ source/R5T.Larissa.Configuration/Code/Services/Definitions/ISvnversionExecutableFilePathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Larissa.Configuration
+{
+    public interface ISvnversionExecutableFilePathProvider
+    {
+        string GetSvnversionExecutableFilePath();
+    }
+}
